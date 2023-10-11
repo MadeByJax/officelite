65f13de35ffddddac7cda154e8b73a53df958be9
MadeByJax <134830936+MadeByJax@users.noreply.github.com>
2023-10-11 22:52:35 +1000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './components/Header'
+import Plans from './components/Plans'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div >
+      <Header />
+      <Plans />
+     </div>
+    </>
+  )
+}
+
+export default App
