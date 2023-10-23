ea108a3ebb2e92f4a0f289bfaa8ac7ef609638d1
MadeByJax <134830936+MadeByJax@users.noreply.github.com>
2023-10-23 19:12:49 +1100

    added navbar component to the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,14 @@
 import React from "react";
 import "./Header.css";
 import charts from "../assets/illustration-charts.svg";
-import logo from "../assets/logo.svg";
+
 import circles from "../assets/bg-pattern-header.svg";
 
 const Header = () => {
   return (
-    <div className="header container">
-      <header>
-        <div className="header__container">
-          <img className="header__logo" src={logo} alt="" />
-        </div>
-      </header>
+    <div className="header">
 
-      <section className="hero__section">
+      <section className="hero__section container">
 
         <img className="hero__charts" src={charts} alt="" />
         <div className="hero__info">
