f058f5f6811fa799b22b856f9c1c20917c5700b9
MadeByJax <134830936+MadeByJax@users.noreply.github.com>
2023-11-02 13:20:49 +1100

    update: anchor tag

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <div className="navbar">
       <header className="container">
         <div className="header__container">
-          <a href="#/officelite">
+          <a href="#/">
             <img className="header__logo" src={logo} alt="" />{" "}
           </a>
         </div>
