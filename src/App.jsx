595c0064cad451f1019966818289bf1c30083ed5
MadeByJax <134830936+MadeByJax@users.noreply.github.com>
2023-11-02 11:52:24 +1100

    add: react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import Header from "./components/Header";
-import Plans from "./components/Plans";
-import Timer from "./components/Timer";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Homescreen from "./pages/Homescreen";
 import Signup from "./pages/Signup";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      
-      <Signup />
-    </>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Homescreen />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
