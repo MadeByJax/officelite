7c1df77ec64cf12c07502b66733b8be9dcc0b7a1
MadeByJax <134830936+MadeByJax@users.noreply.github.com>
2023-11-02 13:34:42 +1100

    fix: display bug on tablet

--- src/components/Plans.jsx
+++ src/components/Plans.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Plans.css";
 import Timer from "./Timer";
+import { Link } from "react-router-dom";
 
 const Plans = () => {
   return (
@@ -12,60 +13,59 @@ const Plans = () => {
             <h2 className="card__price">Free</h2>
             <p>Up to 5 users for free</p>
 
-            <a href="#/signup">
-              <button className="card__button tablet__button">
-                Try for Free
-              </button>
-            </a>
+            <button className="card__button tablet__button">
+              <Link to="/signup">Try for Free</Link>
+            </button>
           </div>
           <ul className="features">
             <li>Basic document collaboration</li>
             <li>2 GB storage</li>
             <li>Great Security and support</li>
           </ul>
-          <a href="#/signup">
-            <button className="card__button">Try for Free</button>
-          </a>
+
+          <button className="card__button">
+            <Link to="/signup">Try for Free</Link>
+          </button>
         </div>
         <div className="card popular">
           <div className="card__left">
             <h3 className="card__tier">Pro</h3>
             <h2 className="card__price">$9.99</h2>
             <p>Per user, billed monthly</p>
-            <a href="#/signup">
-              <button className="card__button tablet__button">
-                Try for Free
-              </button>
-            </a>
+
+            <button className="card__button tablet__button">
+              <Link to="/signup">Try for Free</Link>
+            </button>
           </div>
           <ul className="features">
             <li>All essential integrations</li>
             <li>50 GB storage</li>
             <li>More control and insights</li>
           </ul>
-          <a href="#/signup">
-            <button className="card__button">Try for Free</button>
-          </a>
+
+          <button className="card__button">
+            <Link to="/signup">Try for Free</Link>
+          </button>
         </div>
         <div className="card">
           <div className="card__left">
             <h3 className="card__tier">Ultimate</h3>
             <h2 className="card__price">$19.99</h2>
             <p>Per user, billed monthly</p>
-            <a href="#/signup">
-              <button className="card__button tablet__button">
-                Try for Free
-              </button>
-            </a>
+
+            <button className="card__button tablet__button">
+              <Link to="/signup">Try for Free</Link> Try for Free
+            </button>
           </div>
           <ul className="features">
             <li>Robust work management</li>
             <li>100 GB storage</li>
             <li>VIP support</li>
           </ul>
-          <a href="#/signup">
-            <button className="card__button">Try for Free</button>
-          </a>
+
+          <button className="card__button">
+            <Link to="/signup">Try for Free</Link>
+          </button>
         </div>
       </div>
     </div>
